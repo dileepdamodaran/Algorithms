41ab5a255942d41a919e7a342c81572e75082e34
ddn <dileep.damodaran.n@gmail.com>
2021-01-16 19:36:27 -0500

    Remove vowels from string

---
+++ VowelReplace.java
@@ -0,0 +1,9 @@
+public class VowelReplace {
+
+    public static void main(String args[]) {
+        String s = "leetcodetry";
+        s = s.replaceAll("[aeiou]", "");
+        System.out.println(s);
+    }
+
+}

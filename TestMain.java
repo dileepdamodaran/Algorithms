822825e121498c2406d123e2ea8797a408143273
ddn <dileep.damodaran.n@gmail.com>
2021-02-21 13:32:16 -0500

    Added class to test java code

---
+++ TestMain.java
@@ -0,0 +1,9 @@
+public class TestMain {
+    public static void main(String args[]) {
+        int i = 0;
+        while (i < 6) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}

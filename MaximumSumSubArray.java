7ee888566e2ec93ae13bc0b8eae62a5aa076d03b
ddn <dileep.damodaran.n@gmail.com>
2021-03-14 15:01:37 -0400

    Maximum sub subarray

---
+++ MaximumSumSubArray.java
@@ -0,0 +1,34 @@
+/**
+ * Given an array of positive numbers and a positive number ‘k,’ find the
+ * maximum sum of any contiguous subarray of size ‘k’.
+ */
+public class MaximumSumSubArray {
+
+    private static int findMaxSumSubArray;
+
+    public static void main(String args[]) {
+        int[] arr = new int[] { 2, 1, 5, 1, 3, 2 };
+        int k = 3;
+        int max = findMaxSumSubArray(arr, k);
+        System.out.println(max);
+    }
+
+    public static int findMaxSumSubArray(int[] arr, int k) {
+        int[] subArr = new int[] {};
+        int windowSum = 0;
+        int windowStart = 0;
+        int maxSum = 0;
+        // add sum if i <k
+        // if i>k add sum only if current sum > previous sum
+        // if sum is greater replace first with the recent one
+        for (int i = 0; i < arr.length; i++) {
+            windowSum += arr[i];
+            if (i >= k - 1) {
+                maxSum = Math.max(maxSum, windowSum);
+                windowSum -= arr[windowStart];
+                windowStart++;
+            }
+        }
+        return maxSum;
+    }
+}

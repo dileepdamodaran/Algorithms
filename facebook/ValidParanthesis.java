0546e83e97ffd673bdfbd1270897b835608da532
ddn <dileep.damodaran.n@gmail.com>
2021-03-16 21:12:53 -0400

    Valid paranthesis

--- facebook/ValidParanthesis.java
+++ facebook/ValidParanthesis.java
@@ -9,22 +9,23 @@ public class ValidParanthesis {
 
     public static void main(String args[]) {
         String s = "()";
-        isValid(s);
+        System.out.println(isValid(s));
     }
 
     private static boolean isValid(String s) {
         Stack<Character> stack = new Stack();
 
+        if (s.length() % 2 != 0)
+            return false;
         for (char c : s.toCharArray()) {
-            if (c == '{' || c == '[' || c == '(') {
-                stack.push(c);
-            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            }
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
         }
         return stack.isEmpty();
     }
